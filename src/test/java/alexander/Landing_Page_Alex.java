a0ec051e4130801fc7fe4093afa9713891a3c6bb
Alex <pustovoyalexander@gmail.com>
2017-12-09 12:29:32 +0200

    new commit page object

---
+++ src/test/java/alexander/Landing_Page_Alex.java
@@ -0,0 +1,28 @@
+package alexander;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+public class Landing_Page_Alex {
+
+    WebDriver driver = null;
+    String enterbtn_id = "headEnterBtn";
+    String popupLoginButton_xpath = "/html/body/div[8]/div[2]/div/div[2]/div/div[2]/button[2]";
+
+    public Landing_Page_Alex(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickEnterButton() {
+        WebElement enterbtn = driver.findElement(By.id(enterbtn_id));
+        enterbtn.click();
+    }
+
+    public boolean atLoginPage() {
+        WebElement popupLoginButton = driver.findElement(By.xpath(popupLoginButton_xpath));
+        return (popupLoginButton.isDisplayed());
+    }
+
+}
---
+++ src/test/java/alexander/Login_Page_Alex.java
@@ -0,0 +1,37 @@
+package alexander;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+
+
+
+public class Login_Page_Alex {
+
+    WebDriver driver = null;
+    String loginEmail_xpath = "//*[@id=\"loginForm\"]/div/div[1]/input";
+    String loginPassword_xpath = "//*[@id=\"loginForm\"]/div/div[2]/input";
+    String loginbtn_xpath = "//*[@id=\"loginForm\"]/button";
+
+    public Login_Page_Alex(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+    public void enterLoginEmail() {
+        WebElement loginEmail = driver.findElement(By.xpath(loginEmail_xpath));
+        loginEmail.sendKeys("pustovoyalexander@gmail.com");
+    }
+
+    public void enterPassword() {
+        WebElement loginPassword = driver.findElement(By.xpath(loginPassword_xpath));
+        loginPassword.sendKeys("Qwerty12!");
+    }
+
+    public void clickLogin() {
+        WebElement loginbtn = driver.findElement(By.xpath(loginbtn_xpath));
+        loginbtn.click();
+    }
+
+}
