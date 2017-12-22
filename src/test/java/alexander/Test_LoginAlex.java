5b7e506d39eabf83f820035610688ff338964bb3
Alex <pustovoyalexander@gmail.com>
2017-12-22 03:01:01 +0200

    adding untracked files for previous commit

--- src/test/java/alexander/Test_LoginAlex.java
+++ src/test/java/alexander/Test_LoginAlex.java
@@ -1,9 +1,10 @@
 package alexander;
 
-import alexander.Landing_Page_Alex;
+import alexander.pages.Dashboard_Page_Alex;
+import alexander.pages.Landing_Page_Alex;
+import alexander.pages.Login_Page_Alex;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
